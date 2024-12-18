2e1cb8e1b67426a792f94d8d836b6579a15af03b
Douglas Silva <douglasitpro@gmail.com>
2024-12-18 10:34:49 -0300

    Adicionando o form no admin do django, subscrito

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Users
+from django.contrib.auth import admin as admin_auth_django    #precisa ser diferenciado do admin do django
+from .forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+@admin.register(Users)
+class UserAdmin(admin_auth_django.UserAdmin):    #classe que da o formato do admin como vemos
+    form = UserChangeForm                        #informamos o form criado sobrescrito
+    add_form = UserCreationForm                  #informamos o form criado sobrescrito
+    model = Users                                #infomramos o forme que será utilizado(personalizado)
