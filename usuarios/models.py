7831b741d62274aa65e6a70a56104daa689887cd
Douglas Silva <douglasitpro@gmail.com>
2024-12-18 10:08:58 -0300

    Criando Users apartir da classe User do django 'AbstractUser'

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,1 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+class Users(AbstractUser):
+    choice_cargo = (('V', 'Vendedor'),
+                    ('G', 'Gerente'))
+    cargo = models.CharField(max_length=1, choices=choice_cargo)
