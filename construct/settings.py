313fd2678ab5611129bb1aae0f61ee0ed420d0ae
Douglas Silva <douglasitpro@gmail.com>
2024-12-18 10:38:16 -0300

    Informando ao django o model de autenticao a utilizar

--- construct/settings.py
+++ construct/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'construct.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-         'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,3 +131,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+#Configurando autenticação com as class subscritas(importante)
