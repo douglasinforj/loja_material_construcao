f576558506439bcf6d97d9e822025086fde7490b
Douglas Silva <douglasitpro@gmail.com>
2024-12-18 19:32:29 -0300

    Adicionando decorator para informar o tipo de permissão para acesso a views

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rolepermissions.decorators import has_permission_decorator     #importamos as permissoes criadas no arquivo role
 
 
-
-
+@has_permission_decorator('cadastra_vendedor')      #informamos qual a permissão o usuário que tiver poderá acessar
 def cadastrar_vendedor(request):
     return HttpResponse('Teste')
