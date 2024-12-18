0f38af83e399f60c4fc0894d13469b89114e178d
Douglas Silva <douglasitpro@gmail.com>
2024-12-18 19:55:00 -0300

    Resolvendo problema referente ao campo usable_password: Type:    FieldError Exception Value: Unknown field(s) (usable_password) specified for Users. Check fields/fieldsets/exclude attributes of class UserAdmin.

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,3 +5,4 @@ class Users(AbstractUser):
     choices_cargo = (('V', 'Vendedor'),
                     ('G', 'Gerente'))
     cargo = models.CharField(max_length=1, choices=choices_cargo)
+    usable_password = models.CharField(max_length=255, null=True, blank=True) 
