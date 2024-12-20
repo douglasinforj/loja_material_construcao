f5b5ea4ca8056ba8892c191b7eb69b6e44462e83
Douglas Silva <douglasitpro@gmail.com>
2024-12-20 10:19:05 -0300

    Ajustando erro escrita, falta da virgula entre as rotas

--- construct/urls.py
+++ construct/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('usuarios.urls'))
+    path('auth/', include('usuarios.urls')),
     path('estoque/', include('estoque.urls'))
 ]
 
