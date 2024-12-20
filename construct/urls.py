e7147ecde7ebbfdbab492f34e004f9eb327fa41f
Douglas Silva <douglasitpro@gmail.com>
2024-12-20 09:48:06 -0300

    Adicionando App estoque a urls do projeto

--- construct/urls.py
+++ construct/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('usuarios.urls'))
+    path('estoque', include('estoque.urls'))
 ]
 
 if settings.DEBUG: 
