ee8732b6b6e2d3594b98681f5d60f4c04bb950c7
Douglas Silva <douglasitpro@gmail.com>
2024-12-20 11:17:16 -0300

    Configurando a views para recuperar dados de categoria para o contexto na pagina add_produtos

--- estoque/views.py
+++ estoque/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Categoria
 
 def add_produto(request):
-    return render(request, 'estoque/add_produto.html')
+    #tipos de requisição
+    if request.method == "GET":
+        categorias = Categoria.objects.all()
+        return render(request, 'estoque/add_produto.html', {'categorias': categorias })
